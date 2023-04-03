df6f164c4542dfcd7b9407da442483beeff7f7b1
Shaheer Khan NS <khanshaheer43@gmail.com>
2023-04-04 01:07:24 +0530

    Connected to mySql Database using sequelize

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const path = require("path");
+const bodyParser = require("body-parser");
+
+dotenv.config({ path: "config.env" });
+
+const sequelize = require("./util/database");
 
 // Starting our App
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.json());
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, `public/${req.url}`));
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`App running on ${port}`);
-});
+sequelize
+  .sync()
+  .then(
+    app.listen(port, () => {
+      console.log(`App running on ${port}`);
+    })
+  )
+  .catch((err) => console.log(err));
