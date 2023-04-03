c9e6f573a819a8717a6047a0cbf90a11701f256e
Shaheer Khan NS <khanshaheer43@gmail.com>
2023-04-04 00:38:47 +0530

    Designed add camera form in frontend

--- public/source.js
+++ public/source.js
@@ -1,1 +1,16 @@
+// HTML Elements
+const btnAddCamera = document.getElementById("add-camera");
+const btnCloseCamera = document.getElementById("btn-close-camera");
+const tableContainer = document.querySelector(".camera-tables");
+const addCameraForm = document.querySelector(".camera-add-form ");
 
+btnAddCamera.addEventListener("click", () => {
+  tableContainer.classList.add("hide");
+  addCameraForm.classList.remove("hide");
+});
+
+btnCloseCamera.addEventListener("click", (e) => {
+  e.preventDefault();
+  tableContainer.classList.remove("hide");
+  addCameraForm.classList.add("hide");
+});
